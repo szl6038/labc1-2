8a55ad4afd51e6d33bdc73a59d38c30cb5e62905
szl6038 <szl6038@psu.edu>
2020-09-03 14:27:13 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  double cel,fah;
+  char* name = readline("Enter temperature in celsius: ");
+  cel = atof (name);
+  fah = (cel*9/5) + 32;
+  printf ("%f° in Celsius is equivalent to %f° Fahrenheit. \n" , cel, fah);
+
+  return 0;
+}
